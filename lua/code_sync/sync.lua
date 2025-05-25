2a5a10b64c1e9d20a7c948bfb27cd4eadab7f2f2
Ayush Garg <gargayush341@gmail.com>
2025-05-25 13:53:51 +0530

    updating file at same relative location

--- lua/code_sync/sync.lua
+++ lua/code_sync/sync.lua
@@ -7,6 +7,7 @@ function M.run(env, mode)
   local protocol = config.get_protocol()
   local remotes = config.get_targets(env)
   local keypath = config.get_key_path()
+  local project_name = config.get_project_name()
 
   if #remotes == 0 then
     vim.notify("No remotes defined for environment: " .. env, vim.log.levels.WARN)
@@ -23,6 +24,10 @@ function M.run(env, mode)
   end
 
   for _, remote in ipairs(remotes) do
+    rel_path = M.get_relative_file_path(local_path, project_name)
+    if #rel_path > 0 then
+      remote = remote .. "/" .. rel_path
+    end
     local cmd = protocols.build_command(protocol, keypath, local_path, remote)
     vim.notify(cmd)
     if cmd then
@@ -32,6 +37,16 @@ function M.run(env, mode)
       vim.notify("Unsupported protocol: " .. protocol, vim.log.levels.ERROR)
     end
   end
+end
+
+function M.get_relative_file_path(full_path, project_name)
+  project_name = M.escape_lua_pattern(project_name)
+  local rel_path = full_path:gsub(".*" .. project_name .. "/", "")
+  return rel_path
+end
+
+function M.escape_lua_pattern(s)
+  return s:gsub("([^%w])", "%%%1")
 end
 
 return M
